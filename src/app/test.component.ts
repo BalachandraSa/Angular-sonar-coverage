da516ecbf8b0604760a60dcffc68d1b72335b50d
balachandrasa <balachandra.saa@gmail.com>
2020-09-02 14:11:00 +0530

    intial commit

---
+++ src/app/test.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './app.component.html'
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
